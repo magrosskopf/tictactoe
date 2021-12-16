e0798c400ae8963d006a54fc21eaf469753484dd
Marius <grosskopf81@gmail.com>
2021-12-16 21:44:39 +0100

    clean the code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,17 +3,17 @@ class Board:
     def __init__(self):
         self.state = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.players = []
-        self.activePlayer = ""
+        self.active_player = ""
     def is_board_full(self):
         for i in self.state:
             if i == 0:
                 return False
         return True
-    def changeActivePlayer(self):
-        if self.activePlayer == self.players[0]:
-            self.activePlayer = self.players[1]
+    def change_active_player(self):
+        if self.active_player == self.players[0]:
+            self.active_player = self.players[1]
         else:
-            self.activePlayer = self.players[0]
+            self.active_player = self.players[0]
     def sign_to_printable(self, sign):
         signs = {
             0: " ",
@@ -53,10 +53,10 @@ class Board:
         print(self.sign_to_printable(self.state[6]) + "|"
             + self.sign_to_printable(self.state[7]) + "|"
             + self.sign_to_printable(self.state[ 8]))
-            
+        
     def makeMove(self, cell):
         if self.is_valid_turn(cell):
-            self.state[cell] = self.activePlayer.symbol
+            self.state[cell] = self.active_player.symbol
             return True
         return False
     def is_valid_turn(self, cell):
@@ -65,7 +65,7 @@ class Board:
         else:
             return False
     def getActivePlayerName(self):
-        return self.activePlayer.name
+        return self.active_player.name
 class Player:
     def __init__(self, symbol):
         self.name = input("What is your name?")
@@ -73,17 +73,18 @@ class Player:
         print("Welcome " + self.name + ". ")
 if __name__ == '__main__':
     board = Board()
-    symbol_x = 1
-    symbol_o = 2
-    playerOne = Player(symbol_x)
-    playerTwo = Player(symbol_o)
+    SYMBOL_X = 1
+    SYMBOL_O = 2
+    playerOne = Player(SYMBOL_X)
+    playerTwo = Player(SYMBOL_O)
     board.players = [playerOne, playerTwo]
     print("Let's start the game!")
     while not board.is_board_full():
         board.print_board()
-        board.changeActivePlayer()
+        board.change_active_player()
         try:
-            cell = int(input("Make your move " + board.getActivePlayerName() + "! Where do you want to make your sign [1-9]: "))
+            cell = int(input("Make your move " + board.getActivePlayerName() + 
+            "! Where do you want to make your sign [1-9]: "))
         except ValueError:
             continue
         cell = cell - 1
@@ -96,11 +97,3 @@ if __name__ == '__main__':
         if board.checkWin():
             print("You wonn, " + board.getActivePlayerName() + "! GW.")
             break
-
-    
-
-    
-
-
-
-
